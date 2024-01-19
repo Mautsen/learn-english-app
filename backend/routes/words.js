ec8fc1c7c982774a70d3374dbdf20e96f8bd8519
Matias Leppänen <matias.joosua@hotmail.com>
2024-01-19 19:39:32 +0200

    Add: console.log for error message

--- backend/routes/words.js
+++ backend/routes/words.js
@@ -102,6 +102,8 @@ wordsRouter.post("/", validateWordData, async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
+      console.log(errors.array());
+
       return res.status(400).json({ errors: errors.array() });
     }
 
